0426dca1fcfcb84d61c9c1b30db087b057c86d68
Timo Brouwer <timobrouwer@Timos-MacBook-Pro.local>
2024-04-18 12:28:42 +0200

    Updated

--- PortFolio2/Freezer/src/main/java/org/freezer/Main.java
+++ PortFolio2/Freezer/src/main/java/org/freezer/Main.java
@@ -1,8 +1,5 @@
 package org.freezer;
 
-/**
- * De hoofdklasse van de applicatie die het startpunt bevat voor de uitvoering.
- */
 public class Main {
     /**
      * De hoofdmethode die de applicatie start. Creëert een instantie van AppMenu en roept de run methode aan om het menu te starten.
