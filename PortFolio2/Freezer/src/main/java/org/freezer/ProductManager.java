9cb8e42459df818eb30f0e44ab4d57d5a1686e3f
Timo Brouwer <timobrouwer@timos-mbp.wireless.hhs.nl>
2024-04-19 14:43:13 +0200

    Updated

--- PortFolio2/Freezer/src/main/java/org/freezer/ProductManager.java
+++ PortFolio2/Freezer/src/main/java/org/freezer/ProductManager.java
@@ -7,26 +7,11 @@ import java.util.Scanner;
  * Omvat methoden voor het toevoegen, verwijderen, tonen, en verzenden van productlijsten.
  */
 public interface ProductManager {
-
-    /**
-     * Voegt een nieuw product toe en genereert een QR-code.
-     * Gebruikt een scanner om invoer van de gebruiker te ontvangen.
-     */
     void addAndGenerateProduct(Scanner scanner);
 
-    /**
-     * Verwijdert een product en de bijbehorende QR-code uit het systeem.
-     * Vereist gebruikersinvoer via de scanner.
-     */
     void removeProductAndQrCode(Scanner scanner);
 
-    /**
-     * Toont een lijst van alle producten in het systeem.
-     */
     void displayProductList();
 
-    /**
-     * Verstuurt de productlijst naar een opgegeven e-mailadres.
-     */
     void sendProductListByEmail(String recipientEmail);
 }
